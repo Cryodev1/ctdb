2deeb2e8f3822f9cb3f802472ae7871d5934abaa
Cryodev1 <85141406+Cryodev1@users.noreply.github.com>
2024-05-14 17:45:56 +0000

    Forgot a couple parts, oopsie!

--- src/query/index.ts
+++ src/query/index.ts
@@ -4,12 +4,16 @@ import getOrganizationByName from "./getOrganizationByName";
 import getOrganizationById from "./getOrganizationById";
 import getAllOrganizations from "./getAllOrganizations";
 
+import getAllEvents from "./getAllEvents";
 import getEventById from "./getEventById";
 import getEventByName from "./getEventByName";
 import getEventsByEdition from "./getEventsByEdition";
+import getEventsByOrganizerId from "./getEventsByOrganizerId";
+import getEventsByOrganizerName from "./getEventsByOrganizerName";
 
 import getAllMatches from "./getAllMatches";
 import getMatchesByPlayer from "./getMatchesByPlayer";
+import getMatchById from "./getMatchById"
 
 import getPlayerByEloName from "./getPlayerByEloName";
 import getPlayerById from "./getPlayerById";
@@ -21,11 +25,15 @@ const query = {
   getOrganizationById,
   getAllOrganizations,
 
+  getAllEvents,
   getEventById,
   getEventByName,
   getEventsByEdition,
+  getEventsByOrganizerId,
+  getEventsByOrganizerName,
 
   getAllMatches,
+  getMatchById,
   getMatchesByPlayer,
 
   getPlayerByEloName,
